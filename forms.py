cc071f17b70f9a0c99db6f9a2ac79463afc53eff
Ali Almeer <ali@Alis-MacBook-Air.local>
2025-01-29 17:30:45 +0300

    Fix: Prevent letters in contact phone numbers (#1)

--- forms.py
+++ forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Regexp
 
 class ContactForm(FlaskForm):
     name = StringField('Name')
-    phone = StringField('Phone')
+    phone = StringField('Phone', validators=[ DataRequired(), 
+    Regexp(r'^\+?\d+$', message="Phone number must contain only numbers and an optional '+' sign.")])
     email = StringField('Email')
     type = SelectField('Type', 
                       choices=[('Personal', 'Personal'), 
