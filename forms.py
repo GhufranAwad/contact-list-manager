3f600f62f7dfeeef67fd54e85941d2b25d90fc2d
Student <student@10233-02.local>
2025-01-29 09:58:41 +0300

    Fix : fixed both creating an empty form  and the delete contact

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 class ContactForm(FlaskForm):
-    name = StringField('Name')
+    name = StringField('Name', validators=[DataRequired(message="Name is required.")])  # Ensures name is filled
     phone = StringField('Phone')
-    email = StringField('Email')
+    email = StringField('Email', validators=[DataRequired(message="Email is required."), Email(message="Invalid email address.")])  # Ensures email is filled and valid
     type = SelectField('Type', 
                       choices=[('Personal', 'Personal'), 
                               ('Work', 'Work'), 
